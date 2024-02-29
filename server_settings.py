fc5cab61a7a3f4f0caa692e4fe19d4c86a3428e9
GoriMeri <amselen@stud.ntnu.no>
2024-02-29 19:25:25 +0100

    Fix args
    Add check if no args

--- server_settings.py
+++ server_settings.py
@@ -1,5 +1,5 @@
 SERVER_PORT = 5555
-SERVER_IP = "10.0.0.4"
+SERVER_IP = "192.168.1.111"
 
 MAX_BACKLOG_CONNECTIONS = 5
 # In case of high traffic, the server will reject new connections if there are more than X queued connections
